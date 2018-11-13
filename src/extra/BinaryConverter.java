eb3a1c12e33224048f6243e3309b92e21bc92d36
Idiot <league@WTS14-4.attlocal.net>
2018-11-12 18:24:31 -0800

    BinaryConverter

---
+++ src/extra/BinaryConverter.java
@@ -0,0 +1,48 @@
+package extra;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class BinaryConverter implements ActionListener {
+
+	public static void main(String[] args) {
+		JButton button = new JButton("button1");
+		button.addActionListener(new BinaryConverter());
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JTextField answer = new JTextField(20);
+
+	}
+
+	String convert(String input) {
+		if (input.length() != 8) {
+			JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+			return "-";
+		}
+		String binary = "[0-1]+"; // must contain numbers in the given range
+		if (!input.matches(binary)) {
+			JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+			return "-";
+		}
+		try {
+			int asciiValue = Integer.parseInt(input, 2);
+			char theLetter = (char) asciiValue;
+			return "" + theLetter;
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+			return "-";
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+}
